2c65bc62abf61da0fca847f0f7fd69fa9fceaf4b
Haiatami <tahoanghai755@gmail.com>
2025-02-17 22:45:20 +0700

    Initial commit

---
+++ src/main/java/com/hoanghai/ecommerce/multivendor/api/EcommerceMultivendorApiApplication.java
@@ -0,0 +1,13 @@
+package com.hoanghai.ecommerce.multivendor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMultivendorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMultivendorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoanghai/ecommerce/multivendor/api/EcommerceMultivendorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanghai.ecommerce.multivendor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMultivendorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
