6b755b02ef80f21fb2345311966a7296a006476a
Haiatami <tahoanghai755@gmail.com>
2025-02-24 17:52:15 +0700

    Setup config

---
+++ src/main/java/com/hoanghai/ecommerce/multivendor/api/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.hoanghai.ecommerce.multivendor.api.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "LKOSnBARN1ct7UzXy3qhwoFFED9IXz7m";
+    public static final String JWT_HEADER="Authorization";
+}
